b5a80fbd8d7e5af69892c2e73a18142e61ba7052
YanLong-111 <2944972991@qq.com>
2022-02-28 19:37:46 +0800

    Official version of the contract

--- contracts/MetaDexSwap.sol
+++ contracts/MetaDexSwap.sol
@@ -11,12 +11,12 @@ contract GameItem is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping(address => bool) public userState;
+    mapping(address => uint8) public userState;
 
     constructor() ERC721("MFI Global Ambassador", "MFI Global Ambassador") {}
 
     function tokenURIs() public pure returns (string memory output) {
-        output = string('{"name": "MFI Global Ambassador", "description": "Rewards for MFI Community Global Ambassador assignments", "image": "https://ipfs.io/ipfs/QmRk5FPuhRRE6nTTP5zEFQM738xZYq92KKGazcBKbbgH9a"}');
+        output = string('{"name": "MFI Global Ambassador", "description": "Rewards for MFI Community Global Ambassador Assignments", "image": "https://ipfs.io/ipfs/QmRk5FPuhRRE6nTTP5zEFQM738xZYq92KKGazcBKbbgH9a"}');
         return output;
     }
 
@@ -27,18 +27,18 @@ contract GameItem is ERC721URIStorage, Ownable {
     */
     function addressList(address[] calldata addrlist) public onlyOwner {
         for (uint64 i = 0; i < addrlist.length; i++) {
-            userState[addrlist[i]] = true;
+            userState[addrlist[i]] = 1;
         }
     }
 
     function awardItem() public returns (uint256){
-        require(userState[msg.sender], "not whitelisted");
+        require(userState[msg.sender] == 1, "not whitelisted");
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURIs());
-
+        userState[msg.sender] = 2;
         return newItemId;
     }
 
