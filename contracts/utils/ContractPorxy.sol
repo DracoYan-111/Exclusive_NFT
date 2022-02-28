8dca46ccd600dcf74a869bb7a95dbfa5a1efe43f
YanLong-111 <2944972991@qq.com>
2022-02-28 18:10:50 +0800

    full code

--- contracts/utils/ContractPorxy.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-
-
-pragma solidity ^0.8.9;
-
-contract ContractProxy is ProxyAdmin {}
